ab7d36c1b7875a770132465467f2ae1918246853
Israel <israel230804vc@gmail.com>
2023-08-23 19:57:35 -0400

    agregando footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+const footer = () => {
+  return (
+    <section className="footer">
+        <div className="footer__container container grid">
+            
+        </div>
+    </section>
+  )
+}
+export default footer
